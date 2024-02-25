af0ecaff0be8d24c4f0ef43ab90be78c2704415d
qmy <782719906@qq.com>
2024-02-25 17:06:23 +0800

    fix(test): fix pytest asyncio event loop config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,18 @@
+import asyncio
+
 import nonebot
+import pytest
 
 
 def pytest_configure(config):
     nonebot.init()
+
+
+# without this, asyncio related tests will fail with:
+#   RuntimeError: Event loop is closed
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
