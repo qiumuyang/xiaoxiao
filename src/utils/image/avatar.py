4add8dc7a1c8319ffa649fc7d60161bd7783e916
qmy <782719906@qq.com>
2024-10-16 16:43:00 +0800

    feat(utils): add avatar local file fallback

--- src/utils/image/avatar.py
+++ src/utils/image/avatar.py
@@ -1,20 +1,29 @@
 from datetime import timedelta
 from io import BytesIO
+from pathlib import Path
 
 from aiohttp import ClientSession, ClientTimeout
 from async_lru import alru_cache
 from PIL import Image
 
 from src.ext import logger_wrapper
+from src.utils.env import inject_env
 
 logger = logger_wrapper(__name__)
 
 
+@inject_env()
 class Avatar:
     """A class to fetch QQ avatar images and maintain a cache of them."""
 
     GROUP_URL = "http://p.qlogo.cn/gh/{id}/{id}/0"
     USER_URL = "http://q1.qlogo.cn/g?b=qq&nk={id}&s=640"
+
+    LOCAL_FALLBACK_DIR = "data/dynamic/avatar"
+    GROUP_PATH = "group/{id}.png"
+    USER_PATH = "user/{id}.png"
+
+    avatar_timeout: float = 3.0
 
     _session: ClientSession | None = None
 
@@ -33,15 +42,21 @@ class Avatar:
         default: Image.Image | None = None,
     ) -> Image.Image | None:
         if cls._session is None:
-            cls._session = ClientSession(timeout=ClientTimeout(total=30))
+            cls._session = ClientSession(timeout=ClientTimeout(
+                total=cls.avatar_timeout))
 
         url = (cls.GROUP_URL if is_group else cls.USER_URL).format(id=id)
+        file = Path(cls.LOCAL_FALLBACK_DIR) / (cls.GROUP_PATH if is_group else
+                                               cls.USER_PATH).format(id=id)
         try:
             avatar = await cls._fetch(cls._session, url)
         except Exception as e:
             type_ = "User" if not is_group else "Group"
             logger.error(f"{type_} {id} avatar fetch failed", e)
-            avatar = None
+            avatar = Image.open(file) if file.exists() else None
+        else:
+            file.parent.mkdir(parents=True, exist_ok=True)
+            avatar.save(file)
         return avatar or default
 
     @staticmethod
