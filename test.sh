2e3881222dcd549127571f5620687deab6b4ee75
qmy <782719906@qq.com>
2025-02-03 14:12:50 +0800

    fix(tests): replace deprecated isort args

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ source .venv/bin/activate
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
 
 # remove unused imports first
-isort -sl .
+isort --force-single-line-imports .
 autoflake --remove-all-unused-imports -i -r .
 isort .
 
