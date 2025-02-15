c909f5f457fce450019b76b6bc9423ec829d3b2f
qmy <782719906@qq.com>
2025-02-15 16:16:06 +0800

    feat(dev_tools): add reply JSON representation handling

--- src/plugins/dev_tools/__init__.py
+++ src/plugins/dev_tools/__init__.py
@@ -9,15 +9,25 @@ from src.ext.on import on_reply
 from src.utils.render_ext.markdown import Markdown
 
 check_message_repr = on_reply("repr", permission=SUPERUSER, block=True)
+check_reply_repr = on_reply("reply", permission=SUPERUSER, block=True)
 
 
 @check_message_repr.handle()
 async def _(state: T_State):
     reply: Reply = state["reply"]
     content = reply.message
-    item = json.dumps(MessageSegment.serialize(content),
-                      ensure_ascii=False,
-                      indent=2)
-    markdown_content = f"```json\n{item}\n```"
+    message_json = json.dumps(MessageSegment.serialize(content),
+                              ensure_ascii=False,
+                              indent=2)
+    markdown_content = f"```json\n{message_json}\n```"
     image = Markdown(text=markdown_content).render().to_pil()
     await check_message_repr.finish(MessageSegment.image(image))
+
+
+@check_reply_repr.handle()
+async def _(state: T_State):
+    reply: Reply = state["reply"]
+    reply_json = reply.model_dump_json(indent=2)
+    markdown_content = f"```json\n{reply_json}\n```"
+    image = Markdown(text=markdown_content).render().to_pil()
+    await check_reply_repr.finish(MessageSegment.image(image))
